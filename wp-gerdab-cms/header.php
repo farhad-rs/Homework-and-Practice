c19c4dc2e00d1749b2a8b0f7f801fc62297c0b31
farhad-rs <farhad.raeesi@gmail.com>
2012-09-01 16:08:09 +0430

    Theme For WordPress
    Theme Name : Gerdab.ir

---
+++ wp-gerdab-cms/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link type="text/css" rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" />
+	<title>گرداب | Gerdab.ir</title>
+</head>
+<body>
+<div class="container" lang="fa" dir="rtl">
+	<div class="header"></div>
+	<div class="menu">
+		<ul>
+			<li><a href="#">صفحه نخست</a></li>
+			<li><a href="#">درباره ما</a></li>
+			<li><a href="#">تماس با ما</a></li>
+			<li><a href="#">پیوندها</a></li>
+			<li><a href="#">دریافت خبرنامه</a></li>
+			<li><a href="#">آرشیو</a></li>
+			<li><a href="#">نسخه موبایل</a></li>
+		</ul>
+	</div>
+	<div class="headnews">
+		<div class="daypic">
+			<!--<img class="pic" src="image/day-pic3.jpg" width="236" height="166" />-->
+			<div class="back"></div>
+			<div class="next"></div>
+			<!--<p class="pdaypic"><a href="#"></a></p>-->
+		</div>
+		<div class="weekpic">
+			<!--<a href="#"><img src="image/week-pic2.jpg" width="174" height="190" /></a>-->
+		</div>
+		<div class="weeknews">
+			<!--<a href="#"><img src="image/weeknews.gif" width="192" height="123" /></a>
+			<p class="titr1">اطلاعیه مرکز بررسی در پی تشویش اذهان عمومی؛</p>
+			<p class="titr2">پروژه گرداب مرجع آمار بازدید کاربران از سایت‌های غیر اخلاقی</p>
+			<p class="matn">با انتشار خبری در خبرگزاری فارس و تشویش اذهان عمومی، "مرکز بررسی جرائم سازمان‌یافته سپاه" در اطلاعیه‌ای به شفاف سازی در خصوص خبر مذکور و محورهای مطرح شده در آن از جمله آمار بازدید سایت‌های مستهجن و ارتش سایبری و پروژه‌های آینده مرکز پرداخته است.</p>-->
+		</div>
+	</div>
